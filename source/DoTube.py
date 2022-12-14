9597d3d6ddb2f224c091161595f7f1ff7ddf034a
simps4simps <savarimani6@gmail.com>
2022-12-14 11:04:33 +0330

    Added changes to downloading API

--- source/DoTube.py
+++ source/DoTube.py
@@ -2,7 +2,9 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 import sys
-from pytube import YouTube
+import youtube_dl
+import os
+import ffmpeg
 
 class Mainwindow(QtWidgets.QWidget):
     def __init__(self):
@@ -37,21 +39,6 @@ class Mainwindow(QtWidgets.QWidget):
                 border: 1px solid #f0f0f0;
             }
         """)
-        self.full_Screen = QtWidgets.QPushButton("", clicked=lambda: self.fullscreen())
-        self.full_Screen.setIcon(QtGui.QIcon("../svg/fullscreen.svg"))
-        self.full_Screen.setStyleSheet("""
-                    QPushButton{
-                        background: #212121;
-                        border: 1px solid #212121;
-                        border-radius: 10px;
-                        width: 20px;
-                        padding: 10px;
-                        margin: 5px;
-                    }
-                    QPushButton:hover{
-                        border: 1px solid #f0f0f0;
-                    }
-                """)
         self.mini = QtWidgets.QPushButton("", clicked=lambda: self.minimize())
         self.mini.setIcon(QtGui.QIcon("../svg/min.svg"))
         self.mini.setStyleSheet("""
@@ -107,6 +94,15 @@ class Mainwindow(QtWidgets.QWidget):
                                 border: 1px solid #f0f0f0;
                             }
                         """)
+        self.status = QtWidgets.QLabel('')
+        self.status.setStyleSheet("""
+                            QLabel{
+                                color: #ff3030;
+                                width: 20px;
+                                padding: 20px;
+                                font-size: 15px;
+                            }
+                        """)
         
         # Hbox And Frame
         self.frame1 = QtWidgets.QFrame(self)
@@ -120,15 +116,15 @@ class Mainwindow(QtWidgets.QWidget):
         self.vbox2.addWidget(self.frame1)
         self.frame1.setStyleSheet('border: 1px solid gray; border-radius: 10px;')
         self.frame1.setFixedHeight(25)
-        self.frame1.setFixedWidth(999)
+        self.frame1.setFixedWidth(1000)
         self.setLayout(self.vbox)
         self.hbox.addWidget(self.exit_button)
-        self.hbox.addWidget(self.full_Screen)
         self.hbox.addWidget(self.mini)
         self.vbox.addLayout(self.hbox)
         self.vbox.addWidget(self.entry_label)
         self.vbox.addWidget(self.entry)
         self.vbox.addWidget(self.submit)
+        self.vbox.addWidget(self.status)
         self.vbox.addWidget(self.framespace)
         self.vbox.setContentsMargins(0,0,0,0)
         self.hbox.setContentsMargins(3, 0, 3, 3)
@@ -141,14 +137,6 @@ class Mainwindow(QtWidgets.QWidget):
 
     def quitprogram(self):
         sys.exit()
-
-    def fullscreen(self):
-        if self.counter == 0:
-            self.setWindowState(QtCore.Qt.WindowMaximized)
-            self.full_Screen.setIcon(QtGui.QIcon("../svg/small.svg"))
-            self.counter = 1
-        else:
-            self.smallscreen()
 
     def smallscreen(self):
         self.setWindowState(QtCore.Qt.WindowNoState)
@@ -176,11 +164,27 @@ class Mainwindow(QtWidgets.QWidget):
             self.move(x-x_w,y-y_w)
             
     def download(self):
-        self.url = self.entry.text()
-        ytl = YouTube(url=self.url)
-        vid = ytl.streams.get_by_itag('22')
-        vid.download()
-        print('downloaded')
+        try:
+            self.status.setText('Downloading please dont close the app when it freezes!')
+            self.url = self.entry.text()
+
+            ydl_opts = {}
+            channel = 1
+            while (channel == int(1)):
+                link_of_the_video = self.url
+                zxt = link_of_the_video.strip()
+            
+                dwl_vid()
+            
+            self.status.setText('Download completed!')
+        except:
+            self.status.setText('Oops! Something went wrong. Maybe your URL?')
+            
+    def dwl_vid():
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([zxt])
+            
+        
         
         
 if __name__ == '__main__':
